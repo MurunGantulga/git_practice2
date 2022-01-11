568d0f72e03aedafeb2cdad801b3f7895e250b3c
MurunGantulga <93171046+MurunGantulga@users.noreply.github.com>
2022-01-11 11:29:16 -0600

    Update TestCase002.java

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -3,7 +3,7 @@ package us100;
 public class TestCase002 {
     public static void main(String[] args) {
         System.out.println("The second day of Git Class");
-        System.out.println(" So far so good...:) ");
+        System.out.println(" Git Practice Day! ");
         System.out.println(" We have one more class left. ");
     }
 }
